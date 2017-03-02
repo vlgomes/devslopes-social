00d33a9b88b167f35a1bcb7736e5c8d01d2942d1
vlgomes <uvlgomes@gmail.com>
2017-03-02 17:03:03 +0000

    Firebase Setup Complete

---
+++ devslopes-social/SignInVC.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  devslopes-social
+//
+//  Created by Vasco Gomes on 02/03/2017.
+//  Copyright © 2017 Vasco Gomes. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
