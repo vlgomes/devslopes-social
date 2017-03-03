e7e65409f05a901cdcb2ceee77adea80169e8a2f
vlgomes <uvlgomes@gmail.com>
2017-03-03 15:18:47 +0000

    Email Authentication Completed

--- devslopes-social/SignInVC.swift
+++ devslopes-social/SignInVC.swift
@@ -12,6 +12,8 @@ import FBSDKLoginKit
 import Firebase
 
 class SignInVC: UIViewController {
+    @IBOutlet var emailField: FancyField!
+    @IBOutlet var passwordField: FancyField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,7 +48,27 @@ class SignInVC: UIViewController {
                 print("VASCO: Succesfully authenticated with Firebase")
             }
         })
+    }
+    
+    @IBAction func signInTapped(_ sender: Any) {
         
+        if let email = emailField.text, let password = passwordField.text{
+            FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, error) in
+                if error == nil {
+                    print("VASCO: Succesfully authenticated with email with Firebase")
+                } else {
+                    print("VASCO: Unable to authenticate with email to Firebase. Creating a user")
+                    
+                    FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error) in
+                        if error != nil {
+                            print("VASCO: Unable to authenticate to Firebase - \(error)")
+                        } else {
+                            print("VASCO: Succesfully authenticated with Firebase")
+                        }
+                    })
+                }
+            })
+        }
     }
 }
 
