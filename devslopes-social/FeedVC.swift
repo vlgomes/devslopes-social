0933c0ef88a96ddd953e57c792e7c0efa3b329ac
vlgomes <uvlgomes@gmail.com>
2017-03-07 12:53:19 +0000

    Completed the parsing of firebase data

--- devslopes-social/FeedVC.swift
+++ devslopes-social/FeedVC.swift
@@ -13,6 +13,8 @@ import SwiftKeychainWrapper
 class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    
+    var posts = [Post]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,8 +22,22 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         tableView.delegate = self
         tableView.dataSource = self
         
+        getPosts()
+    }
+    
+    func getPosts(){
         DataService.dataService.REF_POSTS.observe(.value, with: { (snapshot) in
-            print(snapshot.value)
+            if let snapshot = snapshot.children.allObjects as? [FIRDataSnapshot]{
+                for snap in snapshot{
+                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                        let key = snap.key
+                        let post =  Post(postKey: key, postData: postDict)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            
+            self.tableView.reloadData()
         })
     }
     
@@ -30,10 +46,14 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let post = posts[indexPath.row]
+        
+        print("VASCO: \(post.caption)")
         
         return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
     }
