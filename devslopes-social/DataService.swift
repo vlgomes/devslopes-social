a06ace51796cb3db64421939f75f26f97c62395c
vlgomes <uvlgomes@gmail.com>
2017-03-07 12:26:39 +0000

    Completed getting Firebase data

--- devslopes-social/DataService.swift
+++ devslopes-social/DataService.swift
@@ -35,4 +35,6 @@ class DataService{
     func createFirebaseDBUser(uid:String, userData: Dictionary<String,String>){
         REF_USERS.child(uid).updateChildValues(userData)
     }
+    
+    
 }
--- devslopes-social/FeedVC.swift
+++ devslopes-social/FeedVC.swift
@@ -19,6 +19,10 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+        DataService.dataService.REF_POSTS.observe(.value, with: { (snapshot) in
+            print(snapshot.value)
+        })
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
